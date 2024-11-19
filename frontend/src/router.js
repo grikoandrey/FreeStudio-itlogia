d19e3c8361ab8529e4891b4f6056571ad7332758
grikoandrey <griko.and@gmail.com>
2024-11-19 14:39:27 +0100

    add front part 2&3

---
+++ frontend/src/router.js
@@ -0,0 +1,147 @@
+import {Dashboard} from "./components/dashboard.js";
+import {Login} from "./components/login.js";
+import {Signup} from "./components/signup.js";
+
+export class Router {
+    constructor() {
+        this.titlePageElement = document.getElementById('title-page');
+        this.contentPageElement = document.getElementById('content');
+        this.adminStyleElement = document.getElementById('adminlte_style');
+
+        this.initEvents();
+        this.routes = [
+            {
+                route: '/',
+                title: 'Dashboard',
+                filePAthTemplate: '/templates/dashboard.html',
+                useLayout: '/templates/layout.html',
+                load: () => {
+                    new Dashboard();
+                },
+            },
+            {
+                route: '/404',
+                title: 'Page Not Found',
+                filePAthTemplate: '/templates/404.html',
+                useLayout: false,
+            },
+            {
+                route: '/login',
+                title: 'Authorisation',
+                filePAthTemplate: '/templates/login.html',
+                useLayout: false,
+                load: () => {
+                    document.body.classList.add('login-page');
+                    document.body.style.height = '100vh';
+                    new Login();
+                },
+                unload: () => {
+                    document.body.classList.remove('login-page');
+                    document.body.style.height = 'auto';
+                },
+                styles: ['icheck-bootstrap.min.css'],
+            },
+            {
+                route: '/signup',
+                title: 'Registration',
+                filePAthTemplate: '/templates/signup.html',
+                useLayout: false,
+                load: () => {
+                    document.body.classList.add('register-page');
+                    document.body.style.height = '100vh';
+                    new Signup();
+                },
+                unload: () => {
+                    document.body.classList.remove('register-page');
+                    document.body.style.height = 'auto';
+                },
+                styles: ['icheck-bootstrap.min.css'],
+            },
+        ];
+
+
+    };
+
+    initEvents() {
+        window.addEventListener('DOMContentLoaded', this.activateRoute.bind(this));
+        window.addEventListener('popstate', this.activateRoute.bind(this));
+        // window.addEventListener('hashchange', this.activateRoute.bind(this));
+        document.addEventListener('click', this.openNewRoute.bind(this));
+    }
+
+    async openNewRoute(e) {
+
+        let element = null;
+        if(e.target.nodeName === 'A') {
+            element = e.target;
+        } else if (e.target.parentNode.nodeName === 'A') {
+            element = e.target.parentNode;
+        }
+
+        if (element) {
+            e.preventDefault();
+
+            const url = element.href.replace(window.location.origin, '');
+            if (!url || url === '/#' || url.startsWith('javascript:void(0)')) {
+                return;
+            }
+            const currentRoute = window.location.pathname;
+            history.pushState(null, '', url);
+            await this.activateRoute(null, currentRoute);
+        }
+    }
+
+    async activateRoute(e, oldRoute = null) {
+        if (oldRoute) {
+            const currentRoute = this.routes.find(item => item.route === oldRoute);
+            if(currentRoute.styles && currentRoute.styles.length > 0) {
+                currentRoute.styles.forEach(style => {
+                    document.querySelector(`link[href='/css/${style}']`).remove();
+                });
+            }
+            if (oldRoute.unload && typeof oldRoute.unload === 'function') {
+                oldRoute.unload();
+            }
+        }
+
+        const urlRoute = window.location.pathname;
+        const newRoute = this.routes.find(item => item.route === urlRoute);
+
+        if (newRoute) {
+            if(newRoute.styles && newRoute.styles.length > 0) {
+                newRoute.styles.forEach(style => {
+                    const link = document.createElement('link');
+                    link.rel = 'stylesheet';
+                    link.href = `/css/${style}`;
+                    document.head.insertBefore(link, this.adminStyleElement);
+                });
+            }
+            if (newRoute.title) {
+                this.titlePageElement.innerText = `${newRoute.title} | FL Studio`;
+            }
+            if (newRoute.filePAthTemplate) {
+                // document.body.className = '';
+                let contentBlock = this.contentPageElement;
+                if (newRoute.useLayout) {
+                    this.contentPageElement.innerHTML = await fetch(newRoute.useLayout)
+                        .then(response => response.text());
+                    contentBlock = document.getElementById('content-layout');
+                    document.body.classList.add('sidebar-mini');
+                    document.body.classList.add('layout-fixed');
+                } else {
+                    document.body.classList.remove('sidebar-mini');
+                    document.body.classList.remove('layout-fixed');
+                }
+                contentBlock.innerHTML = await fetch(newRoute.filePAthTemplate)
+                    .then(response => response.text());
+            }
+            if (newRoute.load && typeof newRoute.load === 'function') {
+                newRoute.load();
+            }
+        } else {
+            alert('No route found.');
+            history.pushState(null, '', '/404');
+            await this.activateRoute();
+        }
+    };
+}
---
+++ frontend/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    mode: 'development',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: './index.html',
+    }),
+        new CopyPlugin({
+            patterns: [
+                {from: "./src/templates", to: "templates"},
+                {from: "./src/static/images", to: "images"},
+                {from: "./node_modules/admin-lte/plugins/fontawesome-free/css/all.min.css", to: "css"},
+                {from: "./node_modules/admin-lte/plugins/fontawesome-free/webfonts", to: "webfonts"},
+                {from: "./node_modules/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css", to: "css"},
+                {from: "./node_modules/admin-lte/dist/css/adminlte.min.css", to: "css"},
+                {from: "./node_modules/admin-lte/plugins/jquery/jquery.min.js", to: "js"},
+                {from: "./node_modules/admin-lte/dist/js/adminlte.min.js", to: "js"},
+
+            ],
+        }),
+    ],
+};
